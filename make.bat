d3a47a44053327e54ad4e400f3af4e7f49d9b723
Bruno Muller <muller.bru@gmail.com>
2015-01-29 23:01:42 +0100

    Update make and clean

--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
+cd %1
+D:
 sdasz80 -o crt0.s
 sdasz80 -o bios.s
 sdcc -mz80 -c main.c
@@ -5,3 +7,4 @@ sdcc -mz80 -c dir.c
 sdcc -mz80 -c print.c
 sdcc -mz80 --code-loc 0x250C --data-loc 0x3000 --no-std-crt0 -o kernel.ihx main.rel crt0.rel bios.rel dir.rel print.rel
 makebin -p kernel.ihx kernel.bin
+pause
